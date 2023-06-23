5b93eef731ed95b23397c091af44638be47b2291
VilimRoller <vilim.roller@gmail.com>
2023-06-23 12:00:04 +0200

    Defining http methods

--- utils/rest_api.go
+++ utils/rest_api.go
@@ -12,15 +12,38 @@ func RegisterEndpoints() {
 	fmt.Println("Registering endpoints")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", Home)
+	router.HandleFunc("/Basic-Crud", Home)
+	router.HandleFunc("/Basic-Crud/expenses", GetExpenses).Methods("GET")
+	router.HandleFunc("/Basic-Crud/expenses", AddExpense).Methods("POST")
+	router.HandleFunc("/Basic-Crud/expenses", UpdateExpense).Methods("PUT")
+	router.HandleFunc("/Basic-Crud/expenses", DeleteExpense).Methods("DELETE")
 
-	http.Handle("/", router)
+	http.Handle("/Basic-Crud", router)
 
 	http.ListenAndServe(":8080", router)
 }
 
 func Home(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("Home")
 	fmt.Fprintf(writer, "Api is running!")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func GetExpenses(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "GetExpense")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func AddExpense(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "AddExpense")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func UpdateExpense(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "UpdateExpense")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func DeleteExpense(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "DeleteExpense")
 	writer.WriteHeader(http.StatusOK)
 }
